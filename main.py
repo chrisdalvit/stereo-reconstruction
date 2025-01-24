57fe1485b03a7af24066ff61fcfaa5471175b8c2
Christian Dalvit <chris.dalvit@gmail.com>
2025-01-24 09:35:10 +0100

    move c implementation into main

--- main.py
+++ main.py
@@ -6,33 +6,33 @@ import matplotlib.pyplot as plt
 
 from reconstruction.BM import BM
 from reconstruction.SGM import SGM
+from reconstruction.OpenCV import OpenCV
 from utils import load_images, save_point_cloud, compute_metrics
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--method", type=str, choices=['bm', 'sgm', 'opencv'])
 parser.add_argument("--data", type=str, default="cones/")
 parser.add_argument("--subpixel", action='store_true')
-parser.add_argument("--kernel-size", type=int, default=19)
-parser.add_argument("--max-disp", type=int, default=64)
+parser.add_argument("--kernel-size", type=int, default=15)
+parser.add_argument("--max-disp", type=int, default=56)
 parser.add_argument("--P1", type=int, default=10)
 parser.add_argument("--P2", type=int, default=120)
+parser.add_argument("--language", type=str, default='python', choices=['python', 'c'])
 args = parser.parse_args()
 
 left, right, gt = load_images(args.data)
 
 if args.method == 'bm':
-    matcher = BM(args.kernel_size, args.max_disp, args.subpixel)
+    matcher = BM(args.kernel_size, args.max_disp, args.subpixel, args.language)
 elif args.method == 'sgm':
     matcher = SGM(args.kernel_size, args.max_disp, args.P1, args.P2, args.subpixel)
 elif args.method == 'opencv':
-    matcher = cv.StereoBM_create(numDisparities=args.max_disp, blockSize=args.kernel_size)
-    matcher.setPreFilterType(cv.STEREO_BM_PREFILTER_XSOBEL)
+    matcher = OpenCV(args.kernel_size, args.max_disp)
     
 start = time.time()
 disparity = matcher.compute(np.array(left.convert('L')), np.array(right.convert('L')))
 end = time.time()
-if args.method == 'opencv':
-    disparity = disparity.astype(np.float32) / 4.0
+
 disparity = cv.medianBlur(disparity, 5)
 colors = np.array(left)
 print(f"Computation time: {end-start:.2f}s")
