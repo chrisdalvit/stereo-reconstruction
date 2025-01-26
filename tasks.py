cf6639c7bced1b88c31e3fde6890c2db27890808
Christian Dalvit <chris.dalvit@gmail.com>
2025-01-26 16:12:06 +0100

    use different c files for omp

--- tasks.py
+++ tasks.py
@@ -4,9 +4,9 @@ from invoke import task
 @task
 def build_omp_block_matching(c):
     if platform.system() == "Darwin":
-        c.run("gcc -Xclang -fopenmp -L/opt/homebrew/opt/libomp/lib -I/opt/homebrew/opt/libomp/include -lomp -shared -O3 -o c/block_matching.so -fPIC c/block_matching.c")
+        c.run("gcc -Xclang -fopenmp -L/opt/homebrew/opt/libomp/lib -I/opt/homebrew/opt/libomp/include -lomp -shared -O3 -o c/block_matching.so -fPIC c/omp_block_matching.c")
     elif platform.system() == "Linux":
-        c.run("gcc -fopenmp -shared -O3 -o c/block_matching.so -fPIC c/block_matching.c")
+        c.run("gcc -fopenmp -shared -O3 -o c/block_matching.so -fPIC c/omp_block_matching.c")
 
 @task
 def build_block_matching(c):
